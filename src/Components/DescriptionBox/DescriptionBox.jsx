e75c5be88bc152c2ccd34954d1e0d00a063a089a
SudharsanPandiarajan123 <sudharsanpandiarajan@gmail.com>
2023-10-18 11:34:44 +0530

    Updated code

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import "./DescriptionBox.CSS";
+function DescriptionBox() {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          An ecommerce website is your digital storefront on the internet. It
+          facilitates the transaction between a buyer and seller. It is the
+          virtual space where you showcase products, and online customers make
+          selections. Your website acts as the product shelves, sales staff, and
+          cash register of your online business channel.
+        </p>
+        <p>
+          Online shopping for retail sales direct to consumers via web sites and
+          mobile apps, conversational commerce via live chat, chatbots, and
+          voice assistants
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default DescriptionBox;
